0c0325119524d694453ef0896cc132a80d7528e9
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2024-05-08 10:06:46 +1000

    More good test

--- src/dart_jayse/test/json_value_test.dart
+++ src/dart_jayse/test/json_value_test.dart
@@ -105,6 +105,98 @@ void main() {
       // Check that JSON is preserved
       expect(jsonValueEncode(jsonValue), equals(jsonText));
     });
+
+    test('encode and decode with complex objects', () {
+      const personFieldName = 'person';
+      const personFieldValue = <String, dynamic>{
+        'name': 'John Doe',
+        'age': 30,
+        'isEmployed': true,
+        'address': {
+          'street': '123 Main St',
+          'city': 'New York',
+          'country': 'USA',
+        },
+      };
+
+      const companyFieldName = 'company';
+      const companyFieldValue = {
+        'name': 'Acme Inc.',
+        'founded': 1950,
+        'isPublic': false,
+        'founders': [
+          {'name': 'John Smith', 'age': 70},
+          {'name': 'Jane Doe', 'age': 65},
+        ],
+      };
+
+      final jsonText = '{"$personFieldName":${jsonEncode(personFieldValue)},'
+          '"$companyFieldName":${jsonEncode(companyFieldValue)}}';
+
+      final jsonValue = jsonValueDecode(jsonText) as JsonObject;
+
+      // Check person field
+      expect(jsonValue.value[personFieldName], isA<JsonObject>());
+      final person = jsonValue.value[personFieldName]! as JsonObject;
+      expect(person.value['name'], isA<JsonString>());
+      expect(
+        (person.value['name']! as JsonString).value,
+        equals(personFieldValue['name']),
+      );
+      expect(person.value['age'], isA<JsonNumber>());
+      expect(
+        (person.value['age']! as JsonNumber).value,
+        equals(personFieldValue['age']),
+      );
+      expect(person.value['isEmployed'], isA<JsonBoolean>());
+      expect(
+        (person.value['isEmployed']! as JsonBoolean).value,
+        equals(personFieldValue['isEmployed']),
+      );
+
+      expect(person.value['address'], isA<JsonObject>());
+      final address = person.value['address']! as JsonObject;
+      expect(address.value['street'], isA<JsonString>());
+      expect(
+        (address.value['street']! as JsonString).value,
+        // ignore: avoid_dynamic_calls
+        equals(personFieldValue['address']['street']),
+      );
+      expect(address.value['city'], isA<JsonString>());
+      expect(
+        (address.value['city']! as JsonString).value,
+        // ignore: avoid_dynamic_calls
+        equals(personFieldValue['address']['city']),
+      );
+      expect(address.value['country'], isA<JsonString>());
+      expect(
+        (address.value['country']! as JsonString).value,
+        // ignore: avoid_dynamic_calls
+        equals(personFieldValue['address']['country']),
+      );
+
+      // Check company field
+      expect(jsonValue.value[companyFieldName], isA<JsonObject>());
+      final company = jsonValue.value[companyFieldName]! as JsonObject;
+      expect(company.value['name'], isA<JsonString>());
+      expect(
+        (company.value['name']! as JsonString).value,
+        equals(companyFieldValue['name']),
+      );
+      expect(company.value['founded'], isA<JsonNumber>());
+      expect(
+        (company.value['founded']! as JsonNumber).value,
+        equals(companyFieldValue['founded']),
+      );
+      expect(company.value['isPublic'], isA<JsonBoolean>());
+      expect(
+        (company.value['isPublic']! as JsonBoolean).value,
+        equals(companyFieldValue['isPublic']),
+      );
+
+      // Check that JSON is preserved
+      expect(jsonValueEncode(jsonValue), equals(jsonText));
+    });
   });
 
   group('Unit Tests', () {
